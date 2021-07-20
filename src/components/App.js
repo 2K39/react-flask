e024ce3ba79b303825c9fb2b574442797712384b
2K39 <ahmed.yousif.k@gmail.com>
2021-07-20 20:48:07 +0300

    React

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Nav from './Nav.js';
-import Box from "./Box.js";
-import cat from './media/cat.jpg';
-import logo from './media/logo.jpg'
-import './styles/Style.css'
 
 function App(){
-  let nav = []
 
   return(
-     <div className='column'>
-        <Nav nav={nav} logo={cat}/>
+    <div>
+      
     </div>
 )}
 
