ea567f826c127179ca82923e470277c3c8e17a1b
2K39 <ahmed.yousif.k@gmail.com>
2021-08-12 23:07:19 +0300

    icons

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,13 @@
 import React , {useState} from "react";
 import { Container , Button  , Card , Col , Row  } from 'react-bootstrap';
+import * as Icon  from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
-
   return (
   <Container>
 
+  
   </Container>
   );}
 
